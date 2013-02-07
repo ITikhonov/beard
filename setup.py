8dc815fb6bbf42b3d4941d08717c4bcd770cf402
Ivan Tikhonov <kefeer@brokestream.com>
2013-02-07 18:24:44 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name = "beard",
+    packages = ["beard"],
+    version = "0.8",
+    description = "Computation-less template language",
+    author = "Ivan Tikhonov",
+    author_email = "kefeer@brokestream.com",
+    url = "http://brokestream.com/beard.html",
+    download_url = "http://brokestream.com/beard-0.8.zip",
+    keywords = ["templates"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: Public Domain"
+        ],
+    long_description = """\
+Template language designed to be cross-language.
+"""
+)
